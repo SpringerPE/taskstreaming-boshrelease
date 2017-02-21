9822818ecbbd630af9984b9287def523fb22a5cd
Jose Riguera <jriguera@gmail.com>
2017-02-21 10:41:47 +0100

    mend

---
+++ config/bucket.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Create the bucket for blobs and setup public read access
+BUCKET="taskstreaming-boshrelease"
+
+s3cmd mb "s3://${BUCKET}"
+s3cmd setacl "s3://${BUCKET}"  --acl-public --recursive
